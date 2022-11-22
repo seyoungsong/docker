582c2dd14ad545c6d878dc81447967e857103220
seyoung <seyoung@windows>
2022-11-23 03:00:41 +0900

    a

--- README.sh
+++ README.sh
@@ -9,3 +9,9 @@ docker run -it --rm seyoung20/cron-rclone
 
 # inspect
 docker inspect seyoung20/cron-rclone >temp.json
+
+# reset docker
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+docker rmi -f $(docker images -aq)
+docker system prune --all --force
