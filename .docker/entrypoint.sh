87c6568640985c575c7afd1817c1e5e3693b438c
seyoung <seyoung@windows>
2022-11-23 21:42:20 +0900

    copy rclone config

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,6 +4,11 @@
 cp /cron* /app
 dos2unix /app/*
 
+# copy rclone config
+cp /rclone.conf /root/.config/rclone/rclone.conf
+chmod 0644 /root/.config/rclone/rclone.conf
+dos2unix /root/.config/rclone/rclone.conf
+
 # crontab
 chmod 0644 /app/crontab.sh
 crontab /app/crontab.sh
