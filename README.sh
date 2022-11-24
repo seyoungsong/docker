d568b66e98b6421a37f9e852a888822bd5d389fc
seyoung <seyoung@windows>
2022-11-24 23:34:14 +0900

    mv

--- README.sh
+++ README.sh
@@ -1,15 +1,15 @@
 # build
-docker build --tag ghcr.io/seyoungsong/rclone-crontab --file .docker/Dockerfile .docker
+docker build --tag ghcr.io/seyoungsong/rclone --file .docker/Dockerfile .docker
 
 # test bash
-docker run -it --rm ghcr.io/seyoungsong/rclone-crontab /bin/bash
+docker run -it --rm ghcr.io/seyoungsong/rclone /bin/bash
 
 # test cron
-docker run -it --rm ghcr.io/seyoungsong/rclone-crontab
+docker run -it --rm ghcr.io/seyoungsong/rclone
 
 # inspect
-docker pull ghcr.io/seyoungsong/rclone-crontab
-docker inspect ghcr.io/seyoungsong/rclone-crontab >temp.json
+docker pull ghcr.io/seyoungsong/rclone
+docker inspect ghcr.io/seyoungsong/rclone >temp.json
 
 # reset docker
 docker stop $(docker ps -aq)
