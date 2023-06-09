cc6ebefab5bbd8fc26ce2ee530bface463acfb14
seyoung <seyoung@macos>
2023-06-10 04:18:53 +0900

    r

--- .docker/ros-noetic/usage.sh
+++ .docker/ros-noetic/usage.sh
@@ -5,5 +5,7 @@ docker compose pull
 docker compose up --remove-orphans --detach &&
     docker compose logs --follow
 
+docker exec -it ros bash
+
 xhost +local:root # linux
 xhost +localhost  # macos
