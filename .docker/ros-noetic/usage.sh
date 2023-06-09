0ad8b12d5a18a24711feec4e17aada44795466df
seyoung <seyoung@macos>
2023-06-10 04:26:26 +0900

    r

--- .docker/ros-noetic/usage.sh
+++ .docker/ros-noetic/usage.sh
@@ -2,11 +2,12 @@ cd ./.docker/ros-noetic
 
 docker compose pull
 
-docker compose up --remove-orphans --detach &&
-    docker compose logs --follow
+docker compose up --remove-orphans --detach
 
 docker compose stop
 docker compose restart
+
+docker compose down
 
 docker exec -it ros bash
 
