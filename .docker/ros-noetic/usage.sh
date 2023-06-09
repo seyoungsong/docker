180037f28135862476bbc5558954342bbada7684
seyoung <seyoung@macos>
2023-06-10 04:31:41 +0900

    r

--- .docker/ros-noetic/usage.sh
+++ .docker/ros-noetic/usage.sh
@@ -3,13 +3,14 @@ cd ./.docker/ros-noetic
 docker compose pull
 
 docker compose up --remove-orphans --detach
+docker compose ps
+
+docker exec -it ros bash
 
 docker compose stop
 docker compose restart
 
-docker compose down
-
-docker exec -it ros bash
+docker compose down # !WARNING! remove all containers
 
 xhost +local:root # linux
 xhost +localhost  # macos
